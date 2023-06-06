a8ed9b77189b6e0ce9769812e87a7dbfce46479a
kwuunnn <e0958174@u.nus.edu>
2023-06-06 12:24:55 +0800

    create events page

--- frontend-mobile-app/app/(home)/eventsPage.jsx
+++ frontend-mobile-app/app/(home)/eventsPage.jsx
@@ -1,41 +1,32 @@
-import {View, StatusBar, TouchableOpacity, Stylesheet} from "react-native";
+import { View, StatusBar, TouchableOpacity, Stylesheet } from "react-native";
 import { supabase } from "../../lib/supabase";
-import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { Avatar, Button, Card, Text } from "react-native-paper";
 import { Link } from "expo-router";
 
-
-
 export default function ProfileScreen() {
-    return (<Card>
+  return (
+    <Card>
+      <Card.Content>
+        <Text variant="titleLarge">TFAMILY CHALET</Text>
+        <Text variant="bodyMedium">Time: All Day • Venue: Everywhere </Text>
+      </Card.Content>
 
-        <Card.Content>
-          <Text variant="titleLarge">TFAMILY CHALET</Text>
-          <Text variant="bodyMedium">Time: All Day • Venue: Everywhere </Text>
-        </Card.Content>
+      <TouchableOpacity>
+        <Card.Cover
+          source={require("frontend-mobile-app/assets/tfamily.jpg")}
+        />
+      </TouchableOpacity>
 
-        <TouchableOpacity>
-        <Card.Cover source={require("frontend-mobile-app/assets/tfamily.jpg")} />
-        
-        </TouchableOpacity>
-
-        <TouchableOpacity>
+      <TouchableOpacity>
         <Card.Actions>
           <Button>I&apos;m in</Button>
-          <Link href="../components/eventsDesc"> 
-          <Button>View More </Button>
+          <Link href="../components/eventsDesc">
+            <Button>View More </Button>
           </Link>
         </Card.Actions>
-        </TouchableOpacity>
-
-      </Card> )
-        
+      </TouchableOpacity>
+    </Card>
+  );
 }
-    
-    
+
 // <Card.Cover source={{ uri: "file:///Users/saiutkarsh33/Desktop/Nusevents/frontend-mobile-app/assets/tfamily.jpg" }} />
-
-
-
-
-
-
