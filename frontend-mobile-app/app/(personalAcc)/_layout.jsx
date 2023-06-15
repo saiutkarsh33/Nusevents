f7518c8ae0c776681b5a78076d167b281ad0cf6b
kwuunnn <e0958174@u.nus.edu>
2023-06-16 02:23:05 +0800

    home page set as view events

--- frontend-mobile-app/app/(personalAcc)/_layout.jsx
+++ frontend-mobile-app/app/(personalAcc)/_layout.jsx
@@ -3,8 +3,7 @@ import { Tabs } from "expo-router";
 export default function HomeScreen() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="eventsPage" options={{ title: "View Events" }} />
+      <Tabs.Screen name="index" options={{ title: "View Events" }} />
       <Tabs.Screen name="eventsCalendar" options={{ title: "My Calendar" }} />
       <Tabs.Screen name="profile" options={{ title: "Settings" }} />
     </Tabs>
